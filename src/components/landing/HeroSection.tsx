8afba401f938e3d33056ec370509ef42337af6f7
rehlatoursid-umrah <rehlatours.id@gmail.com>
2025-11-24 20:55:33 +0200

    Update HeroSection.tsx

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -23,7 +23,7 @@ const heroData: HeroData = {
   },
   secondaryCTA: {
     text: 'Lihat Video',
-    href: '#video',
+    href: 'https://www.youtube.com/watch?v=8f1JjFzirZ0',
   },
   backgroundImage:
     'https://images.unsplash.com/photo-1579305796538-03268c05b65c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
